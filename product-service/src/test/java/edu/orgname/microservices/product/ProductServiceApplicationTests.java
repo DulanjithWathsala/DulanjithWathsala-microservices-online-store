8395dfe09d2e4ef1624b8a5f7b362ddf904df6ca
M.D.W.De Silva <wdulanjith@gmail.com>
2025-04-05 23:55:16 +0530

    integration test added for create product api

--- product-service/src/test/java/edu/orgname/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/edu/orgname/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,56 @@
 package edu.orgname.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+					{
+						"name": "iPhone 15",
+						"description": "iPhone is a smartphone from Apple",
+						"price": 1000
+					}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product/v1")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 15"))
+				.body("description", Matchers.equalTo("iPhone is a smartphone from Apple"))
+				.body("price", Matchers.equalTo(1000));
 	}
 
 }
