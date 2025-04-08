bfbc538d14994a691403ece300a583f8818443be
M.D.W.De Silva <wdulanjith@gmail.com>
2025-04-08 13:56:11 +0530

    api gateway added to forward requests to downstream microservices

---
+++ api-gateway/src/main/java/edu/orgname/microservices/gateway/routes/GatewayRoutes.java
@@ -0,0 +1,35 @@
+package edu.orgname.microservices.gateway.routes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class GatewayRoutes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return route("product_service")
+                .route(RequestPredicates.path("/api/product/v1"), http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return route("order_service")
+                .route(RequestPredicates.path("/api/order/v1"), http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory/v1"), http("http://localhost:8082"))
+                .build();
+    }
+}
